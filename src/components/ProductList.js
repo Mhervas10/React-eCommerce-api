1290491b141e391ba83a6b638cd13fbd40f1becc
mhervas10 <marc.hervas10@gmail.com>
2019-12-03 18:58:43 +0100

    Created components and basics styling

---
+++ src/components/ProductList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Product from './Product';
+
+export default class ProductList extends Component {
+    state= {
+        products:[]
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+            //    <Product></Product>
+            
+        )
+    }
+}
